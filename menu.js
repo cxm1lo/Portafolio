02c0529e48ae64378b0cec6c205c34431c8dfbcc
cxm1lo <edwardlozanop@gmail.com>
2025-02-04 17:22:30 -0500

    fix

--- menu.js
+++ menu.js
@@ -3,27 +3,3 @@ const nav = document.querySelector('.nav');
     window.addEventListener('scroll', function(){
         nav.classList.toggle('activo', window.scrollY > 0);
     })
-
-
-document.getElementById('contactForm').addEventListener('submit', function(event) {
-        event.preventDefault(); // Evita la redirección
-
-        const formData = new FormData(this); // Obtiene los datos del formulario
-
-        fetch('https://formsubmit.co/edwardlozanop@gmail.com', {
-            method: 'POST',
-            body: formData,
-        })
-        .then(response => {
-            if (response.ok) {
-                alert('Formulario enviado con éxito!');
-                this.reset(); // Reinicia el formulario
-            } else {
-                alert('Hubo un problema al enviar el formulario.');
-            }
-        })
-        .catch(error => {
-            console.error('Error:', error);
-            alert('Error al enviar el formulario.');
-        });
-    });
